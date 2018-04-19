39d80626189361e854f173d2b3e0e59712990f82
Jamie McElwain <jgmcelwain@icloud.com>
2018-04-19 12:27:10 +0100

    fix: use const instaed of var, formatting tweaks

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 const VueMouseflow = {
-  createMfq: function() {
+  createMfq: function () {
     window._mfq = window._mfq || []
   },
-  addTrackingScript: function(tracking_key) {
-    var mf = document.createElement('script')
+  addTrackingScript: function (tracking_key) {
+    const mf = document.createElement('script')
     mf.type = 'text/javascript'
     mf.async = true
-    mf.src = `//cdn.mouseflow.com/projects/${tracking_key}.js`
+    mf.src = '//cdn.mouseflow.com/projects/' + tracking_key + '.js'
 
     document.getElementsByTagName('head')[0].appendChild(mf)
   },
-  install: function(Vue, options) {
+  install: function (Vue, options) {
     this.createMfq()
 
     this.addTrackingScript(options.tracking_key)
   },
-  logRouteChange: function(to, options) {
+  logRouteChange: function (to, options) {
     this.createMfq()
 
     const path = options.includeRouteParams === false ? to.matched[to.matched.length - 1].path : to.fullPath
